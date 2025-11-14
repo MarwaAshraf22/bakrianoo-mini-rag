3e1c7fd1d47939a21473057ab607979ff6aedd2b
Hossam Khair <h.khair95@gmail.com>
2025-11-14 14:26:18 +0200

    refactor: refactor code structure for improved readability and maintainability

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
-from routes import core
+from routes import core, data
 
 load_dotenv()
 
 app = FastAPI()
 app.include_router(core.base_router)
+app.include_router(data.data_router)
