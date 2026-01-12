09a39bdcc85da02b8ae4da411c58c2566b370aba
MarwaAshraf22 <marwa.abdelhameed2012@feps.edu.eg>
2026-01-12 13:58:52 +0200

    update generate_unique_file_path

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -21,7 +21,7 @@ class DataController(BaseController):
         
         return True, ResponseSignal.FILE_VALIDADED_SUCCESS.value
 
-    def generate_unique_file_name(self, orig_file_name:str, project_id:str):
+    def generate_unique_file_path(self, orig_file_name:str, project_id:str):
         
         random_key = self.generate_random_string()
         project_path = ProjectController().get_project_path(project_id=project_id)
@@ -34,7 +34,7 @@ class DataController(BaseController):
             random_key = self.generate_random_string()
             new_file_path = os.path.join(project_path,  random_key + "_" + cleaned_file_name)
 
-        return new_file_path
+        return new_file_path, random_key + "_" + cleaned_file_name
 
 
     def get_clean_file_name(self, orig_file_name:str):
--- src/routes/data.py
+++ src/routes/data.py
@@ -33,7 +33,7 @@ async def upload_data(project_id: str, file: UploadFile,
         )
 
     project_dir_path = ProjectController().get_project_path(project_id=project_id)
-    file_path = Data_Controller.generate_unique_file_name(orig_file_name=file.filename, project_id=project_id)
+    file_path, file_id = Data_Controller.generate_unique_file_path(orig_file_name=file.filename, project_id=project_id)
 
     try:
         
@@ -54,6 +54,7 @@ async def upload_data(project_id: str, file: UploadFile,
 
     return JSONResponse(
             content ={
-                "signal" : ResponseSignal.FILE_UPLOAD_SUCCESS.value
+                "signal" : ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+                "file_id": file_id
             }
         )
