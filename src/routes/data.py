b64d3d8500cc683a6d0c2d31bf0b1875f965e6c0
MarwaAshraf22 <marwa.abdelhameed2012@feps.edu.eg>
2026-01-18 03:26:14 +0200

    fix issues with objectid

--- src/routes/data.py
+++ src/routes/data.py
@@ -9,7 +9,11 @@ import logging
 from .schemes.data import ProcessRequest
 from models.ProjectModel import ProjectModel
 from models.ChunkModel import ChunkModel
-from models.db_schemes import DataChunk
+from models.AssetModel import AssetModel
+from models.db_schemes import DataChunk, Asset
+from models.enums.AssetTypeEnum import AssetTypeEnum
+from fastapi.encoders import jsonable_encoder
+
 
 logger = logging.getLogger('uvicorn.error')
 
@@ -63,13 +67,26 @@ async def upload_data(request: Request, project_id: str, file: UploadFile,
             }
         )
 
+    # store the assets into database
+    asset_model = await AssetModel.create_instence(
+        db_client=request.app.db_client
+    )
+
+    asset_resource = Asset(
+    asset_project_id= project.id,
+    asset_type= AssetTypeEnum.FILE.value,
+    asset_name=file_id,
+    asset_size=os.path.getsize(file_path)
+    )
+
+    asset_record = await asset_model.create_asset(asset=asset_resource)
 
     return JSONResponse(
-            content ={
-                "signal" : ResponseSignal.FILE_UPLOAD_SUCCESS.value,
-                "file_id": file_id
-            }
-        )
+    content=jsonable_encoder({
+        "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+        "file_id": asset_record.id,
+    })
+    )
 
 @data_router.post("/process/{project_id}")
 async def process_endpoint(request: Request, project_id: str, process_request: ProcessRequest):
